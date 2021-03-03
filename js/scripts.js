4de62589af1a12f47102de47db46fba21d9a7802
powills <paigeowills@gmail.com>
2021-03-02 21:48:36 -0500

    added elements to HTML, CSS & JS
    integrated a hamburger icon into the HTML for mobile screens. Updated styling for the header and navigation in CSS & created a collapsing menu in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hello and Welcome to my Portfolio Site!")
+function myFunction() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle'){
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
